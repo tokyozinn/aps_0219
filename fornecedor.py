b1d26ed389ff86abc18ccf78e5dcb17d36f95e2c
tokyozinn <andre.yabiku@hotmail.com>
2019-10-30 13:15:29 -0300

    Implementada a função de busca filtrada por região, uso da biblioteca 're'

--- fornecedor.py
+++ fornecedor.py
@@ -1,12 +1,15 @@
 from user import *
+import re
 
 class Fornecedor(User):
 
     lista_de_fornecedores = []
     
-    def __init__(self, nome, telefone, empresa):
+    def __init__(self, nome, telefone, empresa, endereco, regiao):
         super(Fornecedor, self).__init__(nome, telefone)
         self.empresa = empresa
+        self.endereco = endereco
+        self.regiao = regiao
     
     def altera_cadastro(self, nome, telefone, empresa):
         self.nome = nome
@@ -31,6 +34,17 @@ class Fornecedor(User):
         nome = input('Nome novo: ')
         telefone = input('Telefone novo: ')
         empresa = input('Empresa nova: ')
+        endereco = input('Endereço: ')
+        regiao = input('Região: ')
         arquivo = open('fornecedores.csv', 'a')
-        arquivo.write(f'\r{nome}, {telefone}, {empresa}')
+        arquivo.write(f'{nome}, {telefone}, {empresa}, {endereco}, {regiao}\r')
         arquivo.close()
+
+    def listar_filtrado(self, regiao_informada):
+        i = 0
+        while i < len(Fornecedor.lista_de_fornecedores):
+            if re.search(regiao_informada, Fornecedor.lista_de_fornecedores[i].regiao, re.IGNORECASE):
+                print(Fornecedor.lista_de_fornecedores[i].nome)
+            i += 1
+            continue
+
