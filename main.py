98fb0fac943b167db2c7753899f6130d3aa4759c
tokyozinn <andre.yabiku@hotmail.com>
2019-10-30 13:16:13 -0300

    Ajustes de posição e inserindo função de busca filtrada

--- main.py
+++ main.py
@@ -8,28 +8,34 @@ while(escolha != "0"):
     Fornecedor.cria_usuarios('fornecedores.csv')
 
     escolha = input("""Você deseja:
-                    \n- [1]Procurar locais de descarte;
-                    \n- [2]Acessar como fornecedor   
-                    \n- [3]Acessar como admin
-                    \n- [0]Fechar
+                    \n- [1] Procurar locais de descarte;
+                    \n- [2] Acessar como fornecedor   
+                    \n- [3] Acessar como admin
+                    \n- [0] Fechar
                     \n>>> """)
 
     if escolha == "1":
         while escolha == "1":
             regiao = input('Digite em qual região de Campinas quer encontrar pontos de descarte: ')
             print(f'*******\n\nProcurando locais de descarte na região {regiao}...\n\n********\n')
-            print('Executando Fornecedores.listar()')
+            Fornecedor.listar_filtrado(Fornecedor, regiao)
             print('Deseja procurar outra região?')
-            escolha = input('Digite [1] para SIM, digite [0] para FECHAR >>> ')
+            escolha = input("""\n- [1] SIM
+                               \n- [0] FECHAR >>> """)
     elif escolha == "2":
-        novo = input('Você é fornecedor novo? [Y] or [N]')
+        novo = input("""Você é fornecedor novo?\n 
+                        \n- [Y] SIM 
+                        \n- [N] NÃO""")
         if novo == "Y" or "y":
             Fornecedor.adiciona(Fornecedor)
         else:
             Fornecedor.altera_cadastro()
     elif escolha == "3":
         print('Acessando como admin...')
-        escolha_do_adm = input('Você deseja [1]Listar fornecedores\n[2]Adicionar fornecedor\n[3]Remover fornecedor')
+        escolha_do_adm = input("""Você deseja 
+                                \n- [1] Listar fornecedores
+                                \n- [2] Adicionar fornecedor
+                                \n- [3] Remover fornecedor""")
         if escolha_do_adm == "1":
             Fornecedor.listar(Fornecedor)
         elif escolha_do_adm == "2":
