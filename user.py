7abbbc1344e4b24fdd277f13ca095eda016d39e7
tokyozinn <andre.yabiku@hotmail.com>
2019-10-30 12:33:01 -0300

    Deixando metodo cria_usuarios mais generico

--- user.py
+++ user.py
@@ -5,24 +5,14 @@ class User():
         self.telefone = telefone
 
     @classmethod
-    def cria_usuarios(cls, nome_arquivo):
-        users = []
+    def cria_usuarios(cls, nome_arquivo, lista):
         arquivo = open(nome_arquivo, 'r')
+        i = 0
         for user in arquivo:
             valores = user.split(',')
-            users.append(cls(*valores))
-            print(users)
+            lista.append(cls(*valores))
+            i += 1
         arquivo.close()
-        return users
+        return lista
 
-class Fornecedor(User):
-
-    def __init__(self, nome, telefone, empresa):
-        super(Fornecedor, self).__init__(nome, telefone)
-        self.empresa = empresa
     
-    def altera_cadastro(self, nome, telefone, empresa):
-        self.nome = nome
-        self.telefone = telefone
-        self.empresa = empresa
-        
