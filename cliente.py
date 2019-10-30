a24257b36174394580feb9068b33e50dc104c7ae
tokyozinn <andre.yabiku@hotmail.com>
2019-10-30 13:17:44 -0300

    Classe ainda sem uso. Definir se haverá cadastro de clientes

---
+++ cliente.py
@@ -0,0 +1,22 @@
+from user import *
+
+clientes = []
+
+
+class Cliente(User):
+
+    #lista = Cliente.cria_usuarios('clientes.csv')
+
+    def __init__(self, nome, telefone, regiao):
+        super().__init__(nome, telefone)
+        self.regiao = regiao
+    
+def cria_cliente():
+    nome = input('Digite seu nome: ')
+    telefone = input('Digite seu telefone: ')
+    regiao = input('Digite sua região: ')
+    clientes.append(Cliente(nome, telefone, regiao))
+    print(f"Parabéns pelo seu cadastro {nome}!")
+
+
+
